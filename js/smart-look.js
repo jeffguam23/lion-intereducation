f17d0c20d5a6db9069aa46725c464e2a04869924
Jefferson Guaman <44066964+jeffguam23@users.noreply.github.com>
2019-01-23 23:37:20 +0200

    Update smart-look.js

--- js/smart-look.js
+++ js/smart-look.js
@@ -1,8 +1,6 @@
-<script type="text/javascript">
     window.smartlook||(function(d) {
     var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
     var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
     c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
     })(document);
     smartlook('init', '8fd2317a206d4cc7e3aac7c16dd1faec17531017');
-</script>
